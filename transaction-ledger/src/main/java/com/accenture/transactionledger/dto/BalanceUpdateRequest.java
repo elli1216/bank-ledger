a96725e27b881429d2316c0160e1a45d36a6a8b6
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-06-02 20:25:52 +0800

    add validation annotation for amount in BalanceUpdateRequest

--- transaction-ledger/src/main/java/com/accenture/transactionledger/dto/BalanceUpdateRequest.java
+++ transaction-ledger/src/main/java/com/accenture/transactionledger/dto/BalanceUpdateRequest.java
@@ -2,7 +2,10 @@ package com.accenture.transactionledger.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.NotNull;
+
 public class BalanceUpdateRequest {
+    @NotNull(message = "Amount is required")
     private BigDecimal amount;
 
     public BalanceUpdateRequest() {
